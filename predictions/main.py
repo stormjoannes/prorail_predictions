9a4691e6a945c4addbcb5a2b166d11a9affa60be
stormjoannes <stormjoannes10@hotmail.com>
2020-10-27 13:59:01 +0100

    verwijderen van uitschieters

--- predictions/main.py
+++ predictions/main.py
@@ -2,4 +2,10 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error
 
+#inlezen data
 data = pd.read_csv('../files/sap_storing_data_hu_subset.csv', low_memory=False)
+
+#verwijderen van de functieherstellen langer dan 6 uur
+data.drop(data.loc[data['stm_fh_duur'] >= 360].index, inplace=True)
+highNumbers = data["stm_fh_duur"][data["stm_fh_duur"] == 0] #alles boven de 6 uur duur weggooien.
+print(highNumbers)
