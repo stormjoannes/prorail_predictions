a6bddf5e1a3e8954637ace454c454cbac2c69a05
AHipWhale <charlie.choffat@gmail.com>
2020-10-27 14:49:37 +0100

    html templates

--- applicatie/main.py
+++ applicatie/main.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-
 def home():
-    return "Fakka! dit is de main page <h1>HELLO<h1>"
+    return render_template("index.html")
+
+# @app.route("/<name>")
+# def user(name):
+#     return f"Hello {name}!"
+#
+# @app.route("/admin/")
+# def admin():
+#     return redirect(url_for("user", name="Admin"))
 
 if __name__ == "__main__":
     app.run()
