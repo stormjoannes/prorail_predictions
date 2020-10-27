c32fadfa46471e055ddb5e3cda16ddcdcf6f5bbf
stormjoannes <stormjoannes10@hotmail.com>
2020-10-27 11:55:27 +0100

    bestanden toegevoegd in gitignore

--- predictions/main.py
+++ predictions/main.py
@@ -1,1 +1,5 @@
-print("brandon is dik")
+import pandas as pd
+import numpy as np
+from sklearn.metrics import mean_squared_error
+
+data = pd.read_csv('../sap_storing_data_hu_subset.csv', low_memory=False)
