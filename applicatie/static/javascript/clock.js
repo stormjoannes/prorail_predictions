d21ee17355d97ac900a100d0650a55e85c9f94c3
BKBetz <brandonbetz@live.nl>
2020-10-29 14:24:43 +0100

    bijna eerste design af

---
+++ applicatie/static/javascript/clock.js
@@ -0,0 +1,11 @@
+const deg= 6;
+const hr = document.querySelector("#hr");
+const mn = document.querySelector("#mn");
+
+setInterval(() => {
+    let day = new Date();
+    let hh = day.getHours() * 30;
+    let mm = day.getMinutes() * deg;
+    hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+})
