3c40160ba98cf8ad75f345abe3033662e4f30d25
AHipWhale <charlie.choffat@gmail.com>
2020-10-29 15:36:49 +0100

    sessions toevoegen

--- applicatie/main.py
+++ applicatie/main.py
@@ -1,10 +1,24 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request, session
 
 app = Flask(__name__)
+app.secret_key = "StormIsDik"
 
-@app.route("/")
+@app.route("/", methods= ["POST", "GET"])
 def home():
-    return render_template("base.html")
+    if request.method == "POST":
+        user = request.form["locatie"]
+        session["user"] = user
+        return redirect(url_for("user"))
+    else:
+        return render_template("base.html")
+
+@app.route("/user")
+def user():
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
+    else:
+        return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
