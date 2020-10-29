779384eac682af53b89026ee34c25dbb8998ac02
AHipWhale <charlie.choffat@gmail.com>
2020-10-29 10:41:01 +0100

    testen met template inheritance

--- applicatie/main.py
+++ applicatie/main.py
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def home():
     return render_template("test.html")
 
+@app.route("/test")
+def test():
+    return render_template("new.html")
+# @app.route("/<name>")
+# def user(name):
+#     return f"Hello {name}!"
+#
+# @app.route("/admin/")
+# def admin():
+#     return redirect(url_for("user", name="Admin"))
+
 if __name__ == "__main__":
     app.run(debug=True)
