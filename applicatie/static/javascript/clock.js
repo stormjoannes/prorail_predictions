6f56e95f1bab97887b5ccaca94f5515baa81de79
BKBetz <brandonbetz@live.nl>
2020-11-05 23:44:41 +0100

    clock js aangepast

--- applicatie/static/javascript/clock.js
+++ applicatie/static/javascript/clock.js
@@ -4,6 +4,7 @@ const mn = document.querySelector("#mn");
 const th = document.querySelector("#th");
 const tm = document.querySelector("#tm");
 
+// met deze functie maken we een target door te kijken of de datum wel geldig is en door twee nieuwe wijzers te maken die de target tijd geeft
 function create_target(date) {
     var today = new Date();
     var target_date = new Date(date.textContent);
@@ -29,49 +30,6 @@ function create_target(date) {
     else {
         window.alert("Geen datums in het verleden")
     }
-}
-// de target date is de datum die je was meegegeven plus de duur die het model heeft gegeven
-function checkIfTargetValid() {
-    let today = new Date();
-    var targets = createTargetDate();
-    var valid_dates = [];
-
-    if(targets.length <= 0) {
-        return [];
-    }
-
-    // een target moet dezelfde yaar, maand en dezelfde dag hebben om het voor de clock "valid" te noemen
-    else {
-        for(i=0; i < targets.length; i++) {
-            let day = targets[i].getDate();
-            let month = targets[i].getMonth();
-        }
-        return valid_dates;
-    }
-};
-
-// kijk of een target 5 minuten ervan af is zo ja dan maak toon je de target op de clock
-function getClosestTargets() {
-    let today = new Date();
-    var valid_targets = checkIfTargetValid();
-
-    var closest_target = [];
-
-    for(i=0; i < valid_targets.length; i++) {
-        var difference = valid_targets[i] - today;
-        // zo bereken je het verschil...blijkbaar
-        difference = difference/1000;
-        difference = difference/60;
-        var difference_min = Math.floor(difference % 60);
-        if(difference_min <= 5) {
-            closest_target.push(valid_targets[i]);
-        }
-        else {
-            console.log('none');
-        }
-    }
-    console.log(closest_target);
-    return closest_target;
 }
 
 // met behulp van de constante herhaling van de interval kunnen we de wijzers van de klok laten draaien
